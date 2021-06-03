1df1b640d1de90be7b7f9fcf182da7e5d2b48b68
Michael_Quinones <quinonesmichael04@gmail.com>
2021-06-03 12:19:25 +0000

    Add random facts about me when click me button is pressed

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['I am blonde!', 'I am puerto rican!', 'I love learning!', 'I love programming!'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
