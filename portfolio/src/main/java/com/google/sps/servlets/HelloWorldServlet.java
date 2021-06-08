4429a03277d6ba35770d45f058754e2e2b715e93
Michael_Quinones <quinonesmichael04@gmail.com>
2021-06-08 13:24:27 +0000

    Add step 1 change(Name) to servlet

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -13,6 +13,7 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h1>Michael Quinones</h1>");
+    response.getWriter().println("<p>This is my first servlet!</p>");
   }
 }
