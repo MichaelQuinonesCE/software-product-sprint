fd1942b366ee0e54b3ee57a68f0053d02d289b92
Michael_Quinones <quinonesmichael04@gmail.com>
2021-06-13 21:00:44 +0000

    Add fetch function to get JSON messages, and print a random one

--- portfolio/src/main/webapp/fetch.js
+++ portfolio/src/main/webapp/fetch.js
@@ -1,8 +1,29 @@
 
 async function showServletContent(){
     const responseServer = await fetch('/hello');
-    const textResponse = await responseServer.text();
-
+    const My = await responseServer.json();
     const message = document.getElementById('message-container');
-    message.innerText = textResponse;
+   My.innerHTML = '';
+   const random = Math.floor ((Math.random()*3 )+1);
+   if (random ==1){
+     /*message.appendChild(
+      createListElement('Name: ' + My.Name));*/
+      message.innerText = 'Name: '+ My.Name;
+     }
+     else if (random ==2){
+ /* message.appendChild(
+     createListElement('Nickname: ' + My.Nickname));*/
+     message.innerText = 'Name: '+ My.Nickname;
+  }
+  else if (random ==3){
+ /* message.appendChild(
+      createListElement('Place: ' + My.Place));*/
+      message.innerText = 'Name: '+ My.Place;
+  }
+}
+
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
 }
