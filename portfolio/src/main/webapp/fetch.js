00160c33149462d2d59b05b7c6b40389bace37d9
Michael_Quinones <quinonesmichael04@gmail.com>
2021-06-14 21:23:59 +0000

    Add corrections to JS file

--- portfolio/src/main/webapp/fetch.js
+++ portfolio/src/main/webapp/fetch.js
@@ -1,29 +1,23 @@
 
 async function showServletContent(){
     const responseServer = await fetch('/hello');
-    const My = await responseServer.json();
+    const resp = await responseServer.json();
     const message = document.getElementById('message-container');
-   My.innerHTML = '';
-   const random = Math.floor ((Math.random()*3 )+1);
-   if (random ==1){
-     /*message.appendChild(
-      createListElement('Name: ' + My.Name));*/
-      message.innerText = 'Name: '+ My.Name;
-     }
-     else if (random ==2){
- /* message.appendChild(
-     createListElement('Nickname: ' + My.Nickname));*/
-     message.innerText = 'Name: '+ My.Nickname;
+    resp.innerHTML = '';
+    const random = Math.floor ((Math.random()*3)+1);
+
+   if (random==1){
+         message.innerText = 'Name: '+ resp.Name;
+    }
+   else if (random==2){
+         message.innerText = 'Nickname: '+ resp.Nickname;
   }
-  else if (random ==3){
- /* message.appendChild(
-      createListElement('Place: ' + My.Place));*/
-      message.innerText = 'Name: '+ My.Place;
+   else if (random==3){
+         message.innerText = 'Place: '+ resp.Place;
   }
 }
-
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
+   function createListElement(text) {
+        const liElement = document.createElement('li');
+        liElement.innerText = text;
+        return liElement;
 }
